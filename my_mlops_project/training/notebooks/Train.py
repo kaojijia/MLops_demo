420aed2ad6e545e8bd47e2f64fff0b97ee155e68
Your Name <you@example.com>
2025-07-25 14:38:58 +0800

    change model

--- my_mlops_project/training/notebooks/Train.py
+++ my_mlops_project/training/notebooks/Train.py
@@ -58,7 +58,7 @@ dbutils.widgets.text(
 )
 # Unity Catalog registered model name to use for the trained model.
 dbutils.widgets.text(
-    "model_name", "dev.model.my_mlops_project-model", label="Full (Three-Level) Model Name"
+    "model_name", "mikedata.model.miketest", label="Full (Three-Level) Model Name"
 )
 
 # COMMAND ----------
